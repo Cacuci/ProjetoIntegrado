1129a87c4454b6d1f8773381cb46829009be5c9a
cacuci <rafaelcacuci@yahoo.com.br>
2023-07-16 17:07:28 -0300

    Implementacao do servico de Identidade

---
+++ src/Services/Identity/Controllers/IdentityController.cs
@@ -0,0 +1,51 @@
+﻿using Duende.IdentityServer;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    [Route("api/identity")]
+    public class IdentityController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IdentityServerTools _tools;
+
+        public IdentityController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IdentityServerTools tools)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tools = tools;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Login([FromBody] UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(userModel.UserName,
+                                                                  userModel.Password,
+                                                                  false,
+                                                                  false);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Usuário ou senha inválidos");
+            }
+
+            var user = await _userManager.FindByNameAsync(userModel.UserName);
+
+            var token = await _tools.IssueClientJwtAsync(
+                clientId: user.Id,
+                lifetime: 3600);
+
+            return Ok(token);
+        }
+    }
+}
---
+++ src/Services/Identity/Program.cs
@@ -0,0 +1,69 @@
+using Identity;
+using Identity.Context;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new OpenApiInfo { Title = "Identity", Version = "v1", Description = "Aplicação de identidade baseado no padrão OpenID Connect. A documentação encontra-se disponível em /.well-known/openid-configuration, onde você pode encontrar metadados e links para todos os endpoints." });
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(Config.IdentityResources)
+  .AddInMemoryApiScopes(Config.ApiScopes)
+  .AddInMemoryClients(Config.Clients)
+  .AddAspNetIdentity<ApplicationUser>();
+//.AddDeveloperSigningCredential() // not recommended for production - you need to store your key material somewhere secure
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("v1/swagger.json", "Identity");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
