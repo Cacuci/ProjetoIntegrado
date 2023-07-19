641152ed9f27c9f28d0581216cfd4000d1f520ff
cacuci <rafaelcacuci@yahoo.com.br>
2023-07-19 13:42:02 -0300

    Inclusao dos projetos Configuration e Core

---
+++ src/Services/Configuration/Configurarion.Repository/ConfigurationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Configurarion.Repository
+{
+    public class ConfigurationContext : DbContext
+    {
+        public ConfigurationContext(DbContextOptions<ConfigurationContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ConfigurationContext).Assembly);
+        }
+    }
+}
